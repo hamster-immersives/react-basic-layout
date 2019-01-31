70cf9a095bb1cbc1225f14e74ac938d0d7bc5aab
pak <pak@1916.lan>
2019-01-31 11:51:10 -0500

    form class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
 import React, { Component } from 'react';
 
 
+class Form extends Component {
+
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleInput = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.handleSubmit(this.state)
+  }
+
+  render(){
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input onChange={this.handleInput} name="email" />
+        <input onChange={this.handleInput} name="password" />
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
+
 
 
 class App extends Component {
@@ -10,29 +39,19 @@ class App extends Component {
     password: ""
   }
 
-  handleInput = (event) => {
 
+  handleSubmit = (data) => {
+    console.log(data)
     this.setState({
-      [event.target.name]: event.target.value,
+      email: data.email,
+      password: data.password
     })
-
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-
-    console.log(this.state)
-
   }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleInput} name="email" />
-          <input onChange={this.handleInput} name="password" />
-          <button>Submit</button>
-        </form>
+        <Form handleSubmit={this.handleSubmit}/>
         {`My email is: ${this.state.email} and my password is: ${this.state.password}`}
       </div>
     );
